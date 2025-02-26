e320506ced04ea7c882ef7bb8c0edb741575bc95
rjstebner <rjstebner@gmail.com>
2025-02-26 10:16:11 +0900

    buttons working for activating skills

--- test_branch.gd
+++ test_branch.gd
@@ -6,40 +6,49 @@ var recover = ResourceLoader.load("res://Abilities/BasicAbilities/recover.tres")
 var move = ResourceLoader.load("res://Abilities/BasicAbilities/move.tres")
 
 func _ready():
-    # Ensure the resource is loaded correctly
-
+    # Ensure the resources are loaded correctly
     if move:
         print("Move resource loaded")
-        var target1 = $User
-        var tilemap = Vector2(0, 0)
-        
-        if move.has_method("execute"):
-            move.execute(target1, null, tilemap)
-            print("Ability executed")
     else:
         print("Failed to load move resource")
 
-
     if recover:
-        var target1 = $User
-
-        recover.execute(target1, null, null)
-        print("Ability executed")
+        print("Recover resource loaded")
     else:
         print("Failed to load recover resource")
 
     if light_attack:
-        var target1 = $User
-        var target2 = $Target
-
-        # Add custom properties to the target1 and target2 for testing
-        target1.name = "Hero"
-        target2.name = "Enemy"
-        target2.is_enemy = true
-
-        # Execute the ability
-        light_attack.execute(target1, target2, null)
-        
-        print("Ability executed")
+        print("Light attack resource loaded")
     else:
         print("Failed to load light attack resource")
+
+    # Connect button signals to functions
+    $Control/zeroBTN.connect("pressed", Callable(self, "_on_zeroBTN_pressed"))
+    $Control/oneBTN.connect("pressed", Callable(self, "_on_oneBTN_pressed"))
+    $Control/twoBTN.connect("pressed", Callable(self, "_on_twoBTN_pressed"))
+
+func _on_zeroBTN_pressed():
+    var target1 = $User
+    var tilemap = Vector2(0, 0)
+    if move.has_method("execute"):
+        move.execute(target1, null, tilemap)
+        print("Move ability executed")
+
+func _on_oneBTN_pressed():
+    var target1 = $User
+    if recover.has_method("execute"):
+        recover.execute(target1, null, null)
+        print("Recover ability executed")
+
+func _on_twoBTN_pressed():
+    var target1 = $User
+    var target2 = $Target
+
+    # Add custom properties to the target1 and target2 for testing
+    target1.name = "Hero"
+    target2.name = "Enemy"
+    target2.is_enemy = true
+
+    if light_attack.has_method("execute"):
+        light_attack.execute(target1, target2, null)
+        print("Light attack ability executed")
